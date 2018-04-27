25fa30033453cbf67d70ede214c12c9545f171ae
Dominik <dominik.kiefer@t-online.de>
2018-04-27 19:57:48 +0200

    script with timeout

--- timeAll.sh
+++ timeAll.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-dirlevels="environment/levels/"
 dirlevels="environment/levels-1/"
 
 exec 3<> test.log
 for filename in "$dirlevels"*.lvl; do  
-    echo $(basename $filename)
+    echo "current level" $(basename $filename)
     echo $(readlink -f $filename)
-    >&3 echo $(basename $filename)
-    result=$( { time ./environment/client/main < $filename 1>&3 2>&3; } 2>&3 )
+    >&3 echo "current level" $(basename $filename)
+    result=$( { time ./timeout3 -t 2 ./environment/client/main < $filename 1>&3 2>&3; } 2>&3 )
 done
 
 exec 3>&-
