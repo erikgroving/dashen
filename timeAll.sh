8d578306f314f750c630a76ae3dde05f31238c43
Dominik <dominik.kiefer@t-online.de>
2018-04-27 20:25:16 +0200

    parametrized it all + DOCUMENTED :)

--- timeAll.sh
+++ timeAll.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
+#
+# LINUX
+#
+# ./timeAll.sh path-to-levels timeout-in-seconds logfile-to-write-to
+# e.g.:
+# cd into the project folder (dashen)
+# ./timeAll.sh ./environment/levels-1/ 2 test.log 
+#
+#   preparation or in case something does not work:
+# chmod +x timeAll.sh
+# chmod +x timeout3
+# make clean && make
+#
+# have fun, tested and working :)
 
-dirlevels="environment/levels-1/"
+dirlevels="$1"
+executiontime="$2"
+logfilename="$3"
 
-exec 3<> test.log
+if [ -f $logfilename ] ; then
+    rm $logfilename
+fi
+
+exec 3<> $logfilename
 for filename in "$dirlevels"*.lvl; do  
-    echo "current level" $(basename $filename)
+    echo "LVL" $(basename $filename)
     echo $(readlink -f $filename)
-    >&3 echo "current level" $(basename $filename)
-    result=$( { time ./timeout3 -t 2 ./environment/client/main < $filename 1>&3 2>&3; } 2>&3 )
+    >&3 echo "LVL" $(basename $filename)
+    result=$( { time ./timeout3 -t $executiontime ./environment/client/main < $filename 1>&3 2>&3; } 2>&3 )
 done
 
 exec 3>&-
+
+grep -oe 'LVL.*' -e 'Length.*' -e 'user.*' test.log
 
