49dcd9c4ec973aa7c7547c3bcaa5f3fd4dbfc02e
Dominik <dominik.kiefer@t-online.de>
2018-04-27 19:41:25 +0200

    script working, timeout needed

--- timeAll.sh
+++ timeAll.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 dirlevels="environment/levels/"
+dirlevels="environment/levels-1/"
 
+exec 3<> test.log
 for filename in "$dirlevels"*.lvl; do  
     echo $(basename $filename)
     echo $(readlink -f $filename)
+    >&3 echo $(basename $filename)
+    result=$( { time ./environment/client/main < $filename 1>&3 2>&3; } 2>&3 )
 done
+
+exec 3>&-
+
